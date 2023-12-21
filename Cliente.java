c197e3f5020f4b25a9639459236a6192797a8699
pedroMedrano <pedro.medrano@alumnos.upm.es>
2023-12-21 20:57:10 +0100

    He ido marcando lo que he hecho con comentarios para revisarlo por si acaso. Revisar ListaNaves y ListaPortes. PuertoEspacial terminado. generarListaEnvios() no está terminado.

--- Cliente.java
+++ Cliente.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 /**
@@ -47,21 +48,21 @@ public class Cliente {
     }
     // TODO: Devuelve un envío en función de su posición
     public Envio getEnvio(int i) {
-
+        return listaEnvios.getEnvio(i);
     }
     public ListaEnvios getListaEnvios() {
         return listaEnvios;
     }
     // TODO: Añade un envío al cliente
     public boolean aniadirEnvio(Envio envio) {
-
+        return listaEnvios.insertarEnvio(envio);
     }
     public Envio buscarEnvio(String localizador) {
         return listaEnvios.buscarEnvio(localizador);
     }
     // TODO: Elimina el envío de la lista de envíos del pasajero
     public boolean cancelarEnvio(String localizador) {
-
+        return listaEnvios.eliminarEnvio(localizador);
     }
     public void listarEnvios() {
         listaEnvios.listarEnvios();
@@ -82,9 +83,26 @@ public class Cliente {
      * @return Cliente
      */
     public static Cliente altaCliente(Scanner teclado, ListaClientes clientes, int maxEnvios) {
+        String nombre;
+        String apellidos;
+        String email;
+        teclado.nextLine();
+        System.out.print("Inserte su nombre: ");
+        nombre = teclado.nextLine();
+        System.out.print("Inserte sus apellidos: ");
+        apellidos = teclado.nextLine();
+        do{
+            System.out.print("Introduzca el email: ");
+            email = teclado.nextLine();
+            if(correctoEmail(email)==true){
+                System.out.println("Email incorrecto.");
+            }else if(clientes.buscarClienteEmail(email) != null){
+                System.out.println("El email ya existe");
+            }
+        }while(correctoEmail(email) || clientes.buscarClienteEmail(email) !=null);
 
-
-
+        Cliente cliente = new Cliente(nombre,apellidos,email,maxEnvios);
+        clientes.insertarCliente(cliente);
 
         return new Cliente(nombre, apellidos, email, maxEnvios);
     }
@@ -95,15 +113,19 @@ public class Cliente {
      * @param email
      * @return
      */
-
-
-
-
-
-
-
-
     public static boolean correctoEmail(String email) {
+        if(email.contains("@")==false){
+            return true;
+        }else {
+            String[] usuario = email.split("@");
+            if(Objects.equals(usuario[1],"dop.gov")==false){
+                return false;
+            }else if(usuario[0].startsWith(".")||usuario[0].endsWith(".")){
+                return false;
+            }else{
+                return
+            }
+        }
         return false;
     }
 }
